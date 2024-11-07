2f71a983d6e63d82d93b9a4722efa3e2d478690a
YoanPtr <yoan.pontier@gmail.com>
2024-11-07 11:57:23 +0100

    knn correction

--- knn.py
+++ knn.py
@@ -15,24 +15,26 @@ def distance_matrix(data_train, data_test):
     return dists
 
 
-def knn_predict(dists,labels_train,k):
-    """ To predict the model, for each picture of the test set we find the k-smallest distances and then choose the most recurrent label in those """
+def knn_predict(dists, labels_train, k):
+    """To predict the model, for each picture of the test set we find the k-smallest distances and then choose the most recurrent label in those."""
     predicted_labels = []
 
     for i in range(dists.shape[1]):
-        k_nearest = np.argpartition(dists[:, i], k)
-        labels = labels_train[k_nearest[:k]]
+        # Utilisation de argsort pour obtenir les indices des distances triées dans l'ordre croissant
+        sorted_indices = np.argsort(dists[i, :])[:k]
+        labels = labels_train[sorted_indices]
+        
+        # Trouver l'étiquette la plus fréquente parmi les k plus proches voisins
         unique_labels, counts = np.unique(labels, return_counts=True)
-
         most_frequent_label = unique_labels[np.argmax(counts)]
 
         predicted_labels.append(most_frequent_label)
 
     return predicted_labels
 
-def evaluate_knn(data_train,labels_train,data_test,labels_test,k):
+
+def evaluate_knn(dists,labels_train,labels_test,k):
     """ To evaluate the model, for each picture of the test set we compare the result of the model with the test result. Then we have the scoring for this model. """
-    dists = distance_matrix(data_train,data_test)
 
     predicted_labels = knn_predict(dists, labels_train, k)
 
@@ -47,14 +49,15 @@ def evaluate_knn(data_train,labels_train,data_test,labels_test,k):
 if __name__ == "__main__":
 
     path = 'data/cifar-10-batches-py/'
-    data,labels  = read_cifar_test(path)
+    data,labels  = read_cifar(path)
 
     data_train,labels_train,data_test,labels_test = split_dataset(data,labels,0.9)
 
     accuracy = []
+    dists = distance_matrix(data_train,data_test)
     for k in range(1, 21):
-        accuracy.append(evaluate_knn(data_train, labels_train, data_test, labels_test, k))
-
+        accuracy.append(evaluate_knn(dists, labels_train,  labels_test, k))
+        print(accuracy[-1])
     # Plot accuracy vs. k
     plt.figure(figsize=(8, 6))
     plt.plot(range(1, 21), accuracy, marker='o')
